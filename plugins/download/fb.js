2776e3a365a8ec19a6d32741a5a9f627b2d850f0
Romi Muharom <121702894+Leuthra@users.noreply.github.com>
2023-06-03 15:41:18 +0700

    new sendMessage

--- plugins/download/fb.js
+++ plugins/download/fb.js
@@ -1,15 +1,11 @@
 import fetch from 'node-fetch' //Buat ngambil link gambar...
-import pkg from 'whatsapp-web.js' // Untuk BOT
-const { MessageMedia} = pkg 
 import { facebook } from '@xct007/frieren-scraper'
-import { fileTypeFromBuffer } from 'file-type'
 let handler = async (m, { args, usedPrefix, command }) => {
 	if (!args || !args[0]) return m.reply(`Input URL:\n${usedPrefix + command} https://fb.watch/jL5pC4D874`);
 	const data = await facebook.v1(args[0])
 	if (data.error) return m.reply(`${data.message}`);
-	const res = await fetch(data.isHdAvailable ? data.urls[0].hd : data.urls[1].sd)
-	const buff = Buffer.from(await res.arrayBuffer())
-	await m.reply(new MessageMedia((await fileTypeFromBuffer(buff)).mime, buff.toString("base64")))
+	const res = data.isHdAvailable ? data.urls[0].hd : data.urls[1].sd
+	await conn.sendMessage(m.from, res, {quoted: m })
 }
     handler.help = ['fb']
     handler.tags = ['downloader']
